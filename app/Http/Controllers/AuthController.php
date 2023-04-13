26ccbda6535c1b6236b5d3d329b54bf681ac9aca
Hoàng Trương <oscar.truong.goldenowl.@gmail.com>
2023-04-13 10:18:52 +0700

    remove unnecessary fucntion, formating code

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -30,13 +30,13 @@ class AuthController extends Controller
     // Handle logging out
 
     public function logout(Request $request)
-{
-    Auth::logout();
+    {
+        Auth::logout();
  
-    $request->session()->invalidate();
+        $request->session()->invalidate();
  
-    $request->session()->regenerateToken();
+        $request->session()->regenerateToken();
  
-    return redirect('/login');
-}
+        return redirect('/login');
+    }
 }
