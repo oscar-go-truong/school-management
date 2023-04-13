f88069a9fe118856044dadd2be67b6e72f8661fa
Hoàng Trương <oscar.truong.goldenowl.@gmail.com>
2023-04-13 11:38:55 +0700

    add policy for models

---
+++ app/Policies/RequestPolicy.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Policies;
+
+use App\Enums\UserRole;
+use App\Models\Request;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class RequestPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Create a new policy instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function approve(User $user, Request $request) :bool {
+        return $user->role === UserRole::Adminstrator;
+    }
+}
---
+++ app/Policies/UserCoursePolicy.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Policies;
+
+use App\Enums\UserRole;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class UserCoursePolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Create a new policy instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function create(User $user) : bool {
+        return $user->role === UserRole::Adminstrator;
+    }
+
+    public function update(User $user) : bool {
+        return $user->role === UserRole::Adminstrator;
+    }
+}
