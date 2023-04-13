af4bc6bd1a42081684859dbba4e4c94c14c4a4c7
oscar-go-truong <129922585+oscar-go-truong@users.noreply.github.com>
2023-04-13 11:45:00 +0700

    add policy for models (#4)
    Add policy for models

---
+++ app/Policies/ScorePolicy.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Policies;
+
+use App\Enums\UserRole;
+use App\Models\Exam;
+use App\Models\Score;
+use App\Models\User;
+use App\Models\UserCourse;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class ScorePolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Create a new policy instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+    
+    public function update(User $user, Score $score) : bool {
+        if($user->role !== UserRole::Teacher) 
+            return false;
+        $course_id = Exam::where("id", $score->exam_id)->exec()->coure_id;
+        if(!UserCourse::where('course_id',$course_id)->where('user_id',$user->id)->exists())
+            return false;
+        return true;
+    }
+}
